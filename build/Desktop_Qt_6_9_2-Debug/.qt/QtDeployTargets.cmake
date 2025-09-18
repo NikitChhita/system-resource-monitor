fd4247afe0ba743d5b61266b335a9c9c56196368
Nikit Chhita <nikitchhita@gmail.com>
2025-09-18 15:03:18 -0400

    Remove from git but keep locally

--- build/Desktop_Qt_6_9_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_test_FILE /home/nikit-chhita/test/build/Desktop_Qt_6_9_2-Debug/test)
-set(__QT_DEPLOY_TARGET_test_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_9_2-Debug/.qt/deploy_test_6b4eb971d7.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/nikit-chhita/test/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/test-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "/home/nikit-chhita/test/build/Desktop_Qt_6_9_2-Debug/test"
-    GENERATE_QT_CONF
-)
