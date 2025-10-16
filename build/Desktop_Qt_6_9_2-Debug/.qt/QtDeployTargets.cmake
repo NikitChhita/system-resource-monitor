b805cad5ed0b59425e97ccf35a98647d22f78782
vivero24 <vivero24@students.rowan.edu>
2025-10-16 17:34:57 -0400

    Removed build artifacts

--- build/Desktop_Qt_6_9_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE /home/cvdlr17/Desktop/SWE/system-resource-monitor/build/Desktop_Qt_6_9_2-Debug/untitled)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_9_2-Debug/.qt/deploy_untitled_58a8016465.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/cvdlr17/Desktop/SWE/system-resource-monitor/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/untitled-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "/home/cvdlr17/Desktop/SWE/system-resource-monitor/build/Desktop_Qt_6_9_2-Debug/untitled"
-    GENERATE_QT_CONF
-)
