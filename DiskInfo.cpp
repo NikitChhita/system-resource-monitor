0075df97e2e06e92171f233402fb9e116ff2f378
KHyman <kordallhyman@gmail.com>
2025-10-21 15:32:04 -0400

    Fixed out of range error for DiskInfo

--- DiskInfo.cpp
+++ DiskInfo.cpp
@@ -1,36 +1,33 @@
 #include "DiskInfo.h"
+#include <QDateTime>
+#include <QDebug>
 #include <QFile>
+#include <QRegularExpression>
 #include <QTextStream>
 #include <sys/sysinfo.h>
-#include <QDebug>
-#include <QDateTime>
-#include <QRegularExpression>
 
-DiskInfo::DiskInfo(QObject* parent)
-    :QObject(parent)
-    ,readIOPS(0)
-    ,writeIOPS(0)
-    ,readThroughput(0.0)
-    ,writtenThroughput(0.0)
-    ,prevSectorsRead(0)
-    ,prevSectorsWritten(0)
-    ,prevTime(0)
-    ,firstRun(true)
+DiskInfo::DiskInfo(QObject *parent)
+    : QObject(parent)
+    , readIOPS(0)
+    , writeIOPS(0)
+    , readThroughput(0.0)
+    , writtenThroughput(0.0)
+    , prevSectorsRead(0)
+    , prevSectorsWritten(0)
+    , prevTime(0)
+    , firstRun(true)
 
 {
     m_timer = new QTimer(this);
     connect(m_timer, &QTimer::timeout, this, &DiskInfo::updateDiskInfo);
     m_timer->start(1000);
     updateDiskInfo();
-
 }
 
 void DiskInfo::updateDiskInfo()
 {
-
     QFile file("/proc/diskstats");
-    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qDebug() << "Cannot open /proc/diskstats";
         return;
     }
@@ -44,12 +41,10 @@ void DiskInfo::updateDiskInfo()
     long currentSectorsRead = 0;
     long currentSectorsWritten = 0;
 
-    for(int i = 0; i < lines.size(); i++)
-    {
+    for (int i = 0; i < lines.size() - 1; i++) {
         QString line = lines[i];
         QStringList values = line.split(whitespaceRegex, Qt::SkipEmptyParts);
-        if (values[2] == "vda")
-        {
+        if (values[2] == "vda") {
             readIOPS = values[3].toLong();
             writeIOPS = values[7].toLong();
             currentSectorsRead = values[5].toLong();
@@ -60,8 +55,7 @@ void DiskInfo::updateDiskInfo()
 
     long currentTime = QDateTime::currentMSecsSinceEpoch();
 
-    if(firstRun)
-    {
+    if (firstRun) {
         prevSectorsRead = currentSectorsRead;
         prevSectorsWritten = currentSectorsWritten;
         prevTime = currentTime;
@@ -74,8 +68,7 @@ void DiskInfo::updateDiskInfo()
 
     double elapsedTime = (currentTime - prevTime) / 1000.0;
 
-    if (elapsedTime > 0)
-    {
+    if (elapsedTime > 0) {
         long readSectorsDelta = currentSectorsRead - prevSectorsRead;
         long writtenSectorsDelta = currentSectorsWritten - prevSectorsWritten;
 
@@ -90,9 +83,7 @@ void DiskInfo::updateDiskInfo()
         emit updateWrites(writeIOPS);
         emit updateReadThroughput(readThroughput);
         emit updateWriteThroughput(writtenThroughput);
-
     }
-
 }
 
 QString DiskInfo::getDiskInfoString() const
@@ -101,7 +92,9 @@ QString DiskInfo::getDiskInfoString() const
     double writeMBps = writtenThroughput / (1024.0 * 1024.0);
 
     return QString("Reads Completed: %0 Writes Completed: %1\n"
-                   "Read Throughput: %2 MB/s Write Throughput: %3 MB/s").arg(readIOPS).arg(writeIOPS).arg(readMBps, 0, 'f', 2).arg(writeMBps, 0 , 'f', 2);
+                   "Read Throughput: %2 MB/s Write Throughput: %3 MB/s")
+        .arg(readIOPS)
+        .arg(writeIOPS)
+        .arg(readMBps, 0, 'f', 2)
+        .arg(writeMBps, 0, 'f', 2);
 }
-
-
